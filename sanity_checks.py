c626c822128fc28b24eda1d23c9152b061bafaa4
laurabpaulsen <202005791@post.au.dk>
2025-01-15 16:14:15 +0100

    update plots

--- sanity_checks.py
+++ sanity_checks.py
@@ -4,29 +4,33 @@ import pandas as pd
 
 
 def plot_intensity(df):
-    fig, ax = plt.subplots(1, 1, figsize = (10, 6), dpi = 300)
+    fig, ax1 = plt.subplots(1, 1, figsize=(10, 6), dpi=300)
 
+    # primary y-axis: Stimuli intensity
+    # correct responses (of weak)
+    correct = df[(df["correct"] == 1) & (df["intensity"] > 0)] 
+    ax1.scatter(correct["time"], correct["intensity"], c="green", label="Correct response", s=7)
 
-    # correct
-    correct= df[df["correct"] == 1]
-    ax.scatter(correct["time"], correct["intensity"], c = "green", label = "correct response", s = 7)
+    # incorrect responses of weak
+    incorrect = df[(df["correct"] == 0) & (df["intensity"] > 0)]
+    ax1.scatter(incorrect["time"], incorrect["intensity"], c="red", label="Incorrect response", s=7)
 
-    #incorrect 
-    incorrect = df[df["correct"] == 0]
-    ax.scatter(incorrect["time"], incorrect["intensity"], c = "red", label = "incorrect incorrect", s = 7)
+    # target/weak events
+    df_weak = df[df["event_type"] == "target/weak"]
+    ax1.plot(df_weak["time"], df_weak["intensity"], c="k", alpha=0.7, linewidth=1)
 
-    df_weak = df[df["event_type"]== "target/weak"]
-    ax.plot(df_weak["time"], df_weak["intensity"], c = "k", alpha = 0.7, linewidth = 1)
+    ax1.set_ylabel("Stimuli intensity")
+    ax1.set_xlabel("Time (S)")
+    ax1.legend(loc="upper left")
 
-    
+    # secondary y-axis: ISI
+    ax2 = ax1.twinx()
+    ax2.plot(df_weak["time"], df_weak["ISI"], c="gray", alpha=0.7, linewidth=1, linestyle="--")
+    ax2.set_ylabel("Inter-Stimulus Interval (ISI)", color="gray")
+    ax2.tick_params(axis='y', labelcolor="gray")
 
-    ax.set_ylabel("Stimuli intensity")
-    ax.set_xlabel("Time (s)")
-
-    ax.legend()
-
+    # Save the figure
     plt.savefig("fig/QUEST.png")
-
 
 def check_timing(df):
     fig, ax = plt.subplots(1, 1, figsize = (10, 6), dpi = 300)
